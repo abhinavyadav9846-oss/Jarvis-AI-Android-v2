e0ac28c3881df3681085d9f3bfb26b4982a195d8
abhinavyadav9846-oss <abhinavyadav9846@gmail.com>
2026-08-11 10:11:32 +0530

    Create MainActivity.kt

---
+++ App/src/main/java/com/abhinav/jarvis/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.abhinav.jarvis
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this).apply {
+            text = "Hello Abhinav!\n\nJARVIS is starting..."
+            textSize = 24f
+            setPadding(40, 100, 40, 40)
+        }
+
+        setContentView(textView)
+    }
+}
